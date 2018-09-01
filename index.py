1c004f658e2be43c47be65511f3b3cf6a0734356
think2u <42891495+think2u@users.noreply.github.com>
2018-09-01 21:49:00 +0900

    test

---
+++ index.py
@@ -0,0 +1,34 @@
+#!Python
+print("Content-Type: text/html")
+print()
+import cgi, os
+
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+  <h2>{title}</h2>
+  <p>{desc}</p>
+</body>
+</html>
+'''.format(title=pageId, desc=description, listStr=listStr))
